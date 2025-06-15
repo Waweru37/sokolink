3763cf6645536b3a25b8ae5f4fd32feb0991ba2e
Waweru37 <markwaweru37@gmail.com>
2025-06-15 12:06:55 +0300

    Add sokolink.onrender.com to ALLOWED_HOSTS

--- sokolink_backend/sokolink_backend/settings.py
+++ sokolink_backend/sokolink_backend/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-secret'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sokolink.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
